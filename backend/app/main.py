5bed1cc32b0bd2fc161a4cf9340d51bb4a703d52
PiyushG1816 <pg.piyushgpt18@gmail.com>
2025-08-18 12:43:26 +0530

    Main

--- backend/app/main.py
+++ backend/app/main.py
@@ -11,9 +11,8 @@ app.add_middleware(
     allow_origins=["*"], 
     allow_credentials=True,
     allow_methods=["*"],
-    allow_headers=["*"],
-    expose_headers=["*"]
-)
+    allow_headers=["*"]
+    )
 
 # Routes
 app.include_router(router)
